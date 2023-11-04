18441238932ad8d5224413ef9a5fdc4e51cfa88b
S-Syrichko <106162070+S-Syrichko@users.noreply.github.com>
2023-11-04 22:25:12 +0100

    style: Added burger menu and some styles

--- src/layout/AppNavigation/AppNavigation.tsx
+++ src/layout/AppNavigation/AppNavigation.tsx
@@ -1,29 +1,52 @@
 import { NavLink } from "react-router-dom";
 import useGlobalStore from "../../lib/hooks/useGlobalStore";
 import styles from "./AppNavigation.module.scss";
+import { useState } from "react";
+import logo from "../../assets/Logo.svg";
 
 const AppNavigation = () => {
   const { userId } = useGlobalStore();
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
-    <nav className={styles.nav}>
-      <NavLink
-        to="/"
-        className={(navData) => (navData.isActive ? styles.active : "")}
-      >
-        Accueil
-      </NavLink>
-      <NavLink
-        to="/books/add"
-        className={(navData) => (navData.isActive ? styles.active : "")}
-      >
-        Ajouter un livre
-      </NavLink>
-      <NavLink
-        to="/auth"
-        className={(navData) => (navData.isActive ? styles.active : "")}
-      >
-        {userId ? "Se déconnecter" : "Se connecter"}
-      </NavLink>
+    <nav className={`${styles.navbar} ${showLinks ? styles.showLinks : ""}`}>
+      <img className="logo" src={logo} alt="logo"></img>
+      <ul className={styles.navbarLinks}>
+        <li className={`${styles.navbarItem} ${styles.slideIn_1}`}>
+          <NavLink
+            to="/"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+            onClick={showLinks ? toggleMobileMenu :  undefined}
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li className={`${styles.navbarItem} ${styles.slideIn_2}`}>
+          <NavLink
+            to="/books/add"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+            onClick={showLinks ? toggleMobileMenu :  undefined}
+          >
+            Ajouter un livre
+          </NavLink>
+        </li>
+        <li className={`${styles.navbarItem} ${styles.slideIn_3}`}>
+          <NavLink
+            to="/auth"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+            onClick={showLinks ? toggleMobileMenu :  undefined}
+          >
+            Se {userId ? "déconnecter" : "connecter"}
+          </NavLink>
+        </li>
+      </ul>
+      <button className={styles.burgerButton} onClick={toggleMobileMenu}>
+        <span className={styles.burgerButtonBar}></span>
+      </button>
     </nav>
   );
 };
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,8 @@
-import logo from '../../assets/Logo.svg';
 import AppNavigation from '../AppNavigation/AppNavigation';
 import styles from './Header.module.scss';
 const Header = () => {
     return (
         <header className={styles.header}>
-            <img className="logo" src={logo} alt="logo"></img>
             <AppNavigation />
         </header>
     );
